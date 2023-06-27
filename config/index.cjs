c972d9d101e49a10773b54391f31a3dc1fe68221
ling20031222 <113595305+ling20031222@users.noreply.github.com>
2023-06-27 15:52:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '泠宝',
+      name: 'opfz46YKUi0J_BXWWD-jqBv2GXcA',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -53,7 +53,7 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: '小何',
+      name: 'opfz46YKUi0J_BXWWD-jqBv2GXcA',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'opfz46WEZuShA5NVOnuIfuBBB4PA',
     }
